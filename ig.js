dd0d0e33634988120a11a8b948abec3d1596f749
Greyxz <136082654+Greyxzsadz@users.noreply.github.com>
2024-11-22 22:02:49 +0800

    Create ig.js

---
+++ ig.js
@@ -0,0 +1,34 @@
+case 'igvid': case 'igmp4': case 'igdl': case 'ig':{
+    if (!text) return m.reply(`Anda perlu memberikan URL video, reel`);
+   // reply (msg.wait)
+    let res;
+    try {
+        res = await fetch(`https://vkrdownloader.vercel.app/server?vkr=${encodeURIComponent(text)}`);
+    } catch (error) {
+        return m.reply(`An error occurred: ${error.message}`);
+    }
+
+    let api_response;
+    try {
+        api_response = await res.json();
+    } catch (error) {
+        return m.reply(`Failed to parse API response: ${error.message}`);
+    }
+
+    if (!api_response || !api_response.result || api_response.result.length === 0) {
+        return m.reply(`No video or image found or Invalid response from API.`);
+    }
+
+try {
+    const mediaData = api_response.result[0]; // Ambil elemen pertama dari array result
+    //const mediaType = mediaData.thumbnail ? 'image' : 'video'; // Periksa apakah thumbnail ada
+    const mediaURL = mediaData.url;
+    const cap = `HERE IS THE VIDEO`;
+
+            await conn.sendMessage(m.chat, { video: { url: mediaURL }, caption: cap }, { quoted: m });
+        
+    } catch (error) {
+        return m.reply(`Failed to send media: ${error}`);
+    }
+}
+break
