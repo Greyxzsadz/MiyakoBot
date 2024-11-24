f8a9a8caac33f3fa37e5cc14717e7816e2a8543e
Greyxz <136082654+Greyxzsadz@users.noreply.github.com>
2024-11-24 21:55:55 +0800

    Update qc.js

--- qc.js
+++ qc.js
@@ -1,39 +1,197 @@
-case 'qc': {
-if (!q) return reply(`📌Example: ${prefix + command} hallo`)
-if (text.length > 100) return reply(`Max 100 character.`)
+if (typeof plugin !== 'function')
+                continue
+            if ((usedPrefix = (match[0] || '')[0])) {
+                let noPrefix = m.text.replace(usedPrefix, '')
+                let [command, ...args] = noPrefix.trim().split` `.filter(v => v)
+                args = args || []
+                let _args = noPrefix.trim().split` `.slice(1)
+                let text = _args.join` `
+                
+                command = (command || '').toLowerCase()
+                let fail = plugin.fail || global.dfail 
+                let isAccept = plugin.command instanceof RegExp ? 
+                    plugin.command.test(command) :
+                    Array.isArray(plugin.command) ? 
+                        plugin.command.some(cmd => cmd instanceof RegExp ? 
+                            cmd.test(command) :
+                            cmd === command
+                        ) :
+                        typeof plugin.command === 'string' ? 
+                            plugin.command === command :
+                            false
 
-let [...message] = text.split(' ');
-message = message.join(' ');
-await XeonStickWait()
-let obj = {
-type: 'quote',
-format: 'png',
-backgroundColor: '#ffffff',
-width: 512,
-height: 768,
-scale: 2,
-messages: [
-{
-entities: [],
-avatar: true,
-from: {
-id: 1,
-name: pushname,
-photo: { 
-url: await conn.profilePictureUrl(m.sender, "image").catch(() => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'),
+                if (!isAccept)
+                    continue
+                m.plugin = name
+                if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
+                    let chat = global.db.data.chats[m.chat]
+                    let user = global.db.data.users[m.sender]
+                    if (name != 'owner-unbanchat.js' && name != 'owner-exec.js' && name != 'owner-exec2.js' && name != 'tool-delete.js' && chat?.isBanned)
+                        return 
+                    if (name != 'owner-unbanuser.js' && user?.banned)
+                        return
+                }
+                if (plugin.rowner && plugin.owner && !(isROwner || isOwner)) { 
+                    fail('owner', m, this)
+                    continue
+                }
+                if (plugin.rowner && !isROwner) { 
+                    fail('rowner', m, this)
+                    continue
+                }
+                if (plugin.owner && !isOwner) { 
+                    fail('owner', m, this)
+                    continue
+                }
+                if (plugin.mods && !isMods) { 
+                    fail('mods', m, this)
+                    continue
+                }
+                if (plugin.premium && !isPrems) {
+                    fail('premium', m, this)
+                    continue
+                }
+                if (plugin.group && !m.isGroup) { 
+                    fail('group', m, this)
+                    continue
+                } else if (plugin.botAdmin && !isBotAdmin) { 
+                    fail('botAdmin', m, this)
+                    continue
+                } else if (plugin.admin && !isAdmin) { 
+                    fail('admin', m, this)
+                    continue
+                }
+                if (plugin.private && m.isGroup) { 
+                    fail('private', m, this)
+                    continue
+                }
+                if (plugin.register == true && _user.registered == false) {
+                    fail('unreg', m, this)
+                    continue
+                }
+                m.isCommand = true
+                let xp = "exp" in plugin ? parseInt(plugin.exp) : 17 
+                if (xp > 200)
+                    this.reply(m.chat, `[🚩] *Sepertinya Anda Curang Menggunakan Kalkulator*\Buy Limit /buy limit\n\nCheat Limit /ngechit`, )
+                else 
+                m.exp += xp
+                if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
+                    this.reply(m.chat, `[🚩] *Limit Anda Habis Buy Limit Dengan Comand*\n\nBuy Limit /buy limit`,)
+                    continue 
+                }
+                if (plugin.level > _user.level) {
+                    this.reply(m.chat, `[🚩] Diperlukan level ${plugin.level} untuk menggunakan perintah ini\n*Level mu:* ${_user.level} 📊*`,)
+                    continue 
+                }
+                let extra = {
+                    match,
+                    usedPrefix,
+                    noPrefix,
+                    _args,
+                    args,
+                    command,
+                    text,
+                    conn: this,
+                    participants,
+                    groupMetadata,
+                    user,
+                    bot,
+                    isROwner,
+                    isOwner,
+                    isRAdmin,
+                    isAdmin,
+                    isBotAdmin,
+                    isPrems,
+                    chatUpdate,
+                    __dirname: ___dirname,
+                    __filename
+                }
+                try {
+                    await plugin.call(this, m, extra)
+                    if (!isPrems)
+                        m.limit = m.limit || plugin.limit || false
+                } catch (e) {
+                   
+                    m.error = e
+                    console.error(e)
+                    if (e) {
+                        let text = format(e)
+                        for (let key of Object.values(global.APIKeys))
+                            text = text.replace(new RegExp(key, "g"), "#HIDDEN#")
+                        if (e.name)
+                            for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+                                let data = (await conn.onWhatsApp(jid))[0] || {}
+                                if (data.exists)
+                                    return m.reply(`*🗂️ Plugins:* ${m.plugin}\n*👤 Sender:* ${m.sender}\n*💬 Chat:* ${m.chat}\n*💻 Command:* ${usedPrefix}${command} ${args.join(" ")}\n📄 *Error Logs:*\n\n${text}`.trim(), data.jid)
+                            }
+                        m.reply(text)
+                    }
+                } finally {
+                  
+                    if (typeof plugin.after === "function") {
+                        try {
+                            await plugin.after.call(this, m, extra)
+                        } catch (e) {
+                            console.error(e)
+                        }
+                    }
+                    if (m.limit)
+                        m.reply(+m.limit + " ʟɪᴍɪᴛ ᴛᴇʀᴘᴀᴋᴀɪ ✔️")
+                }
+                break
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    } finally {
+        if (opts['queque'] && m.text) {
+            const quequeIndex = this.msgqueque.indexOf(m.id || m.key.id)
+            if (quequeIndex !== -1)
+                this.msgqueque.splice(quequeIndex, 1)
+        }
+     
+        let user, stats = global.db.data.stats
+        if (m) {
+            if (m.sender && (user = global.db.data.users[m.sender])) {
+                user.exp += m.exp
+                user.limit -= m.limit * 1
+            }
+
+            let stat
+            if (m.plugin) {
+                let now = +new Date
+                if (m.plugin in stats) {
+                    stat = stats[m.plugin]
+                    if (!isNumber(stat.total))
+                        stat.total = 1
+                    if (!isNumber(stat.success))
+                        stat.success = m.error != null ? 0 : 1
+                    if (!isNumber(stat.last))
+                        stat.last = now
+                    if (!isNumber(stat.lastSuccess))
+                        stat.lastSuccess = m.error != null ? 0 : now
+                } else
+                    stat = stats[m.plugin] = {
+                        total: 1,
+                        success: m.error != null ? 0 : 1,
+                        last: now,
+                        lastSuccess: m.error != null ? 0 : now
+                    }
+                stat.total += 1
+                stat.last = now
+                if (m.error == null) {
+                    stat.success += 1
+                    stat.lastSuccess = now
+                }
+            }
+        }
+
+        try {
+            if (!opts['noprint']) await (await import(`./lib/print.js`)).default(m, this)
+        } catch (e) {
+            console.log(m, m.quoted, e)
+        }
+        if (opts['autoread'])
+            await this.chatRead(m.chat, m.isGroup ? m.sender : undefined, m.id || m.key.id).catch(() => { })
+    }
 }
-},
-text: message,
-replyMessage: {},
-},
-],
-};
-let response = await axios.post('https://quotly.netorare.codes/generate', obj, {
-headers: {
-'Content-Type': 'application/json',
-},
-});
-let buffer = Buffer.from(response.data.result.image, 'base64');
-conn.sendImageAsSticker(m.chat, buffer, m, { packname: `${global.packname}`, author: `${global.author}`})
-}
-break
