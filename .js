3369dd74286764ff03cbe98e605e408b7b6fb8ba
Greyxz <136082654+Greyxzsadz@users.noreply.github.com>
2024-11-27 20:10:22 +0800

    Create .js

---
+++ .js
@@ -0,0 +1,41 @@
+Silakan salin teks berikut:
+
+
+---
+
+"Ya, halo Furina. Ada keperluan apa panggil aku?"
+
+"Apa benda ni, sial."
+
+"Ya."
+
+"Apa hal panggil-panggil ni?"
+
+"Kau siapa?"
+
+"Bisinglah kau panggil-panggil."
+
+"Siapa ya?"
+
+"Macam mana kau tahu nama aku? Kita kenal ke?"
+
+"Ada apa panggil aku? 🗿"
+
+"Siapa ya? Aku tak kenal."
+
+"Kenapa panggil-panggil ni? -_-"
+
+"Apa kau? -_-"
+
+"Ya, kenapa?"
+
+"Bising kau, anj*ng."
+
+"Bising betul kau panggil-panggil."
+
+
+
+---
+
+Semoga membantu!
+
